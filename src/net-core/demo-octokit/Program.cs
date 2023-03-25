4e1824ef852d488a5301773574a9321ded0f0414
Luis Fraile <lfraile@lfraile.net>
2023-03-25 17:32:23 +0100

    First commit

---
+++ src/net-core/demo-octokit/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Octokit;
+using System.Reflection;
+
+// Build a config object, using env vars and JSON providers.
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+    .Build();
+
+var client = new GitHubClient(new ProductHeaderValue(config["demo-app"]));
+client.Credentials = new Credentials(config["github-token"]);
+
+var repos = await client.Repository.GetAllForOrg(config["organization"]);
+
+repos.ToList().ForEach(repo =>
+{
+    Console.WriteLine(repo.FullName);
+});
